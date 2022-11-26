428fd80b1edab1223797442a84d3aa380f0c02b0
yoojinLiz <yj.oh.dev@gmail.com>
2022-11-26 13:17:31 +0000

    유진 argument passing 하는 함수 분리 (약속 지킴 ^0^)

--- include/userprog/process.h
+++ include/userprog/process.h
@@ -60,7 +60,6 @@ int process_wait (tid_t);
 void process_exit (void);
 void process_activate (struct thread *next);
 
-char * argument_parsing (char *f_name, struct intr_frame *_if);
 struct thread *get_child_process (tid_t child_tid);
 void copy_fd_list(struct thread* parent,struct thread* child);
 
